bb85c1742aef56d32489201169a6f9ab31aee195
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2021-12-10 09:57:07 -0600

    add initial files

---
+++ force-app/main/default/lwc/developertest/developertest.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+//Develop a LWC that is accessible via a Lightning App page and Community Page
+//Display a Lightning Card with a Lightning Button variant of Brand
+//Center the Button (Width) using Lightning Design System
+//Given the context of the component the buttons color should display differently
+    //Community - Red
+    //Lightning Large Flexipage - Blue
+    //Lightning Medium Flexipage - Green
+    //Lightning Small Flexipage - Yellow
+//When the user clicks the button it should return a list of 5 Accounts from the controller DeveloperTestApexController
+//Display the List of Accounts in the UI using Lightning Layout Items
+//Write tests for your Apex Code
+    //Test for a Return of Data
+    //Test for handling of the Exception
+    //Provide 100% code coverage
+
+export default class Developertest extends LightningElement {}
