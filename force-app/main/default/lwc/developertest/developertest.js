3b8c2086b9c91e37db87b5ea65e1643c786203f5
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2021-12-10 13:33:33 -0600

    conditionally rendered button based on component context solution mvp

--- force-app/main/default/lwc/developertest/developertest.js
+++ force-app/main/default/lwc/developertest/developertest.js
@@ -1,18 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track, api } from 'lwc';
+import getAccountList from '@salesforce/apex/DeveloperTestApexController.getAccountList';
 
 //Develop a LWC that is accessible via a Lightning App page and Community Page
 //Display a Lightning Card with a Lightning Button variant of Brand
 //Center the Button (Width) using Lightning Design System
+//When the user clicks the button it should return a list of 5 Accounts from the controller DeveloperTestApexController
+//Write tests for your Apex Code
+    //Test for a Return of Data
+    //Test for handling of the Exception
+//Display the List of Accounts in the UI using Lightning Layout Items
 //Given the context of the component the buttons color should display differently
     //Community - Red
     //Lightning Large Flexipage - Blue
     //Lightning Medium Flexipage - Green
     //Lightning Small Flexipage - Yellow
-//When the user clicks the button it should return a list of 5 Accounts from the controller DeveloperTestApexController
-//Display the List of Accounts in the UI using Lightning Layout Items
-//Write tests for your Apex Code
-    //Test for a Return of Data
-    //Test for handling of the Exception
+    
     //Provide 100% code coverage
 
-export default class Developertest extends LightningElement {}
+export default class Developertest extends LightningElement {
+    @api buttonClassName;
+    @api flexipageRegionWidth;
+    
+    @track clicked = false;
+    @wire(getAccountList) accList;
+
+    connectedCallback(){
+        if(this.flexipageRegionWidth != null && this.flexipageRegionWidth != undefined){
+            this.buttonClassName = this.flexipageRegionWidth;
+        } else {
+            this.buttonClassName = 'COMMUNITY';
+        }
+        
+        console.log(this.buttonClassName);
+    }
+
+    handleClick(){
+        if(this.clicked == true){
+            this.clicked = false;
+        } else if(this.clicked == false){
+            this.clicked = true;
+        }
+    }
+}
