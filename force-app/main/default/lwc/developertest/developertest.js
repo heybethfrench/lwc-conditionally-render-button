b5bc5cb61c83d31f082728fbdbd4e7c1de3ec8d3
Elizabeth French <elizabethfrench@Elizabeths-MacBook-Pro.local>
2021-12-10 13:49:52 -0600

    code clean up

--- force-app/main/default/lwc/developertest/developertest.js
+++ force-app/main/default/lwc/developertest/developertest.js
@@ -1,22 +1,6 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import getAccountList from '@salesforce/apex/DeveloperTestApexController.getAccountList';
 
-//Develop a LWC that is accessible via a Lightning App page and Community Page
-//Display a Lightning Card with a Lightning Button variant of Brand
-//Center the Button (Width) using Lightning Design System
-//When the user clicks the button it should return a list of 5 Accounts from the controller DeveloperTestApexController
-//Write tests for your Apex Code
-    //Test for a Return of Data
-    //Test for handling of the Exception
-//Display the List of Accounts in the UI using Lightning Layout Items
-//Given the context of the component the buttons color should display differently
-    //Community - Red
-    //Lightning Large Flexipage - Blue
-    //Lightning Medium Flexipage - Green
-    //Lightning Small Flexipage - Yellow
-    
-    //Provide 100% code coverage
-
 export default class Developertest extends LightningElement {
     @api buttonClassName;
     @api flexipageRegionWidth;
@@ -30,8 +14,6 @@ export default class Developertest extends LightningElement {
         } else {
             this.buttonClassName = 'COMMUNITY';
         }
-        
-        console.log(this.buttonClassName);
     }
 
     handleClick(){
